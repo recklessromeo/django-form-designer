eec52ca776dadeb2fb44ab99be5b55cb151a7379
Jakob Kosir <kosir.jakob@gmail.com>
2014-09-23 19:31:21 +0200

    template and form code updates

--- form_designer/forms.py
+++ form_designer/forms.py
@@ -1,13 +1,12 @@
-from importlib import import_module
-from djangular.forms.angular_base import TupleErrorList
-from djangular.forms.angular_validation import NgFormValidationMixin
-from djangular.styling.bootstrap3.forms import Bootstrap3FormMixin
 import os
 
+from importlib import import_module
 from django import forms
 from django.forms import widgets
 from django.conf import settings as django_settings
 from django.utils.translation import ugettext as _
+from djangular.forms.angular_validation import NgFormValidationMixin
+from djangular.styling.bootstrap3.forms import Bootstrap3FormMixin
 
 from form_designer import settings
 from form_designer.models import FormDefinitionField, FormDefinition
@@ -16,17 +15,18 @@ from form_designer.utils import get_class
 
 
 class DesignedForm(forms.Form):
-
     def __init__(self, form_definition, initial_data=None, *args, **kwargs):
         super(DesignedForm, self).__init__(*args, **kwargs)
         self.file_fields = []
         for def_field in form_definition.formdefinitionfield_set.all():
             self.add_defined_field(def_field, initial_data)
-        self.fields[form_definition.submit_flag_name] = forms.BooleanField(required=False, initial=1, widget=widgets.HiddenInput)
+        self.fields[form_definition.submit_flag_name] = forms.BooleanField(required=False, initial=1,
+                                                                           widget=widgets.HiddenInput)
 
     def add_defined_field(self, def_field, initial_data=None):
         if initial_data and initial_data.has_key(def_field.name):
-            if not def_field.field_class in ('django.forms.MultipleChoiceField', 'django.forms.ModelMultipleChoiceField'):
+            if not def_field.field_class in (
+            'django.forms.MultipleChoiceField', 'django.forms.ModelMultipleChoiceField'):
                 def_field.initial = initial_data.get(def_field.name)
             else:
                 def_field.initial = initial_data.getlist(def_field.name)
@@ -43,10 +43,10 @@ class DesignedForm(forms.Form):
 class AngularValidationForm(NgFormValidationMixin, Bootstrap3FormMixin, DesignedForm):
     form_error_css_classes = 'djng-form-errors'
     field_error_css_classes = 'djng-field-errors'
-    field_mixins_module = field_mixins_fallback_module = 'djangular.forms.field_mixins'
-    form_name = 'miha'
+    field_mixins_module = import_module('djangular.forms.field_mixins')
 
     def __init__(self, form_definition, *args, **kwargs):
+        self.form_name = self.get_form_name(form_definition)
         super(AngularValidationForm, self).__init__(form_definition, *args, **kwargs)
         self.fields.pop(form_definition.submit_flag_name)
 
@@ -55,18 +55,22 @@ class AngularValidationForm(NgFormValidationMixin, Bootstrap3FormMixin, Designed
 
     def add_defined_field(self, def_field, initial_data=None):
         field = super(AngularValidationForm, self).add_defined_field(def_field, initial_data)
-        field_mixins_module = import_module(self.field_mixins_module)
-        field_mixins_fallback_module = import_module(self.field_mixins_fallback_module)
-        # add additional methods to django.form.fields at runtime
-        FieldMixinName = field.__class__.__name__ + 'Mixin'
+        # add additional methods to django.form.fields at runtime (from django-angular form validation)
+        field_mixin_name = field.__class__.__name__ + 'Mixin'
         try:
-            FieldMixin = getattr(field_mixins_module, FieldMixinName)
+            field_mixin_class = getattr(self.field_mixins_module, field_mixin_name)
         except AttributeError:
-            try:
-                FieldMixin = getattr(field_mixins_fallback_module, FieldMixinName)
-            except AttributeError:
-                FieldMixin = field_mixins_fallback_module.DefaultFieldMixin
-        field.__class__ = type(field.__class__.__name__, (FieldMixin, field.__class__), {})
+            field_mixin_class = self.field_mixins_module.DefaultFieldMixin
+        field.__class__ = type(field.__class__.__name__, (field_mixin_class, field.__class__), {})
+
+    def get_form_name(self, form_definition):
+        """
+        Modify form slug to angular model applicable name
+        """
+        name = form_definition.name.replace('-', '_')
+        if name[:1].isdigit():
+            return 'model' + name
+        return name
 
 
 class FormDefinitionFieldInlineForm(forms.ModelForm):
@@ -74,12 +78,14 @@ class FormDefinitionFieldInlineForm(forms.ModelForm):
         model = FormDefinitionField
 
     def clean_regex(self):
-        if not self.cleaned_data['regex'] and self.cleaned_data.has_key('field_class') and self.cleaned_data['field_class'] in ('django.forms.RegexField',):
+        if not self.cleaned_data['regex'] and self.cleaned_data.has_key('field_class') and self.cleaned_data[
+            'field_class'] in ('django.forms.RegexField',):
             raise forms.ValidationError(_('This field class requires a regular expression.'))
         return self.cleaned_data['regex']
 
     def clean_choice_model(self):
-        if not self.cleaned_data['choice_model'] and self.cleaned_data.has_key('field_class') and self.cleaned_data['field_class'] in ('django.forms.ModelChoiceField', 'django.forms.ModelMultipleChoiceField'):
+        if not self.cleaned_data['choice_model'] and self.cleaned_data.has_key('field_class') and self.cleaned_data[
+            'field_class'] in ('django.forms.ModelChoiceField', 'django.forms.ModelMultipleChoiceField'):
             raise forms.ValidationError(_('This field class requires a model.'))
         return self.cleaned_data['choice_model']
 
@@ -88,7 +94,8 @@ class FormDefinitionForm(forms.ModelForm):
     class Meta:
         model = FormDefinition
 
-    def _media(self):
+    @property
+    def media(self):
         js = []
         plugins = [
             'js/jquery-ui.js',
@@ -105,4 +112,5 @@ class FormDefinitionForm(forms.ModelForm):
         js.extend(
             [os.path.join(settings.STATIC_URL, path) for path in plugins])
         return forms.Media(js=js)
-    media = property(_media)
+
+
